77b13292c0abe86a7239523243dd14e8d9b0dc84
Ali.Saeedifar <ali.saeedifar-ext@doc.coach>
2022-07-09 18:54:36 +0430

    preparing the fadeView component

---
+++ README.md
@@ -0,0 +1,54 @@
+
+# React Native FadeView
+
+ FadeView is a handy component written in [TypeScript](https://www.typescriptlang.org/) in order to handle the fade animations more easier for every react native component.
+
+
+## Installing
+
+For the latest stable version:
+
+using npm:
+```bash
+npm install --save react-native-fadeview-wrapper
+```
+
+using yarn:
+```bash
+yarn add react-native-fadeview-wrapper
+```
+
+## Usage
+
+```tsx
+import React, { Component } from 'react';
+import FadeView from 'react-native-fadeview-wrapper';
+
+const Example = () => {
+
+    const [isVisible, setIsVisible] = React.useState<boolean>(false)
+
+    return (
+      <FadeView visible={isVisible}>
+        {/** any components can be used here in order to have the fade animation */}
+      </FadeView>
+    );
+}
+```
+
+## Documentation
+
+here is the properties and the descriptions of it: 
+
+
+| Props Name | Default | Type | isRequired | Description |
+| :--: | :----- | :--: | :--: | :------------------------- |
+| visible | | **it's required** | `boolean` | `YES` | changing this param would initiate the fade animation |
+| shouldEnterWithAnimation | `false` | `boolean` | `NO` | if it's `true`, then the first state of `visible` would be presented by animation |
+| style | `undefined` | `ViewStyle` | `NO` | optinal style for the fadeview itself |
+| children | `undefined` | `JSX.Element | JSX.Element[]` | `NO` | child component(s) in order to have the fade animation |
+| entranceBearing | `Bearing.Center` | `Bearing` | `NO` | entrance animation with can be determined by `Bearing` enum from the lib, values: `Top`, `Bottom`, `Left`, `Right` , `Center` |
+| leaveBearing | `Bearing.Center` | `Bearing` | `NO` | leave animation with can be determined by `Bearing` enum from the lib, values: `Top`, `Bottom`, `Left`, `Right` , `Center` |
+| fadeOutScale | 1.1 | `number` | `NO` | scale of fade out state of the animation |
+| duration | 200 | `number` | `NO` | milliseconds of the fade animation |
+| animationFinished | `undefined` | `(visible: boolean) => void` | `NO` | call back closure in order to do something after each animation is finished |
---
+++ src/components/FadeView.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import {
+    Animated,
+    Easing,
+    ViewStyle
+} from 'react-native'
+
+export enum Bearing {
+    Top, Bottom, Left, Right, Center
+}
+
+type Props = {
+    visible: boolean
+    style?: ViewStyle,
+    children?: JSX.Element | JSX.Element[],
+    entranceBearing?: Bearing
+    leaveBearing?: Bearing
+    fadeOutScale?: number
+    duration?: number
+    shouldEnterWithAnimation?: boolean
+    animationFinished?: (isVisible: boolean) => void
+    // All other props
+    [x: string]: any
+}
+
+const FadeView = ({ 
+    style, 
+    visible, 
+    children, 
+    shouldEnterWithAnimation, 
+    fadeOutScale = 1.1, 
+    duration = 200, 
+    entranceBearing = Bearing.Center, 
+    leaveBearing = Bearing.Center, 
+    animationFinished, 
+    ...rest 
+}: Props) => {
+
+    const [visibleState, setVisibleState] = React.useState<boolean>(shouldEnterWithAnimation == true ? !visible : visible)
+    const [visibilityAnimValue] = React.useState(new Animated.Value(shouldEnterWithAnimation == true ? (visible ? 0 : 1) : (visible ? 1 : 0)))
+    const theAnimation = React.useRef<Animated.CompositeAnimation | null>(null)
+    const applyingVisibleState = React.useRef<Boolean>(shouldEnterWithAnimation == true ? !visible : visible)
+
+    React.useEffect(() => {
+        if (applyingVisibleState.current == visible) { return }
+        if (theAnimation && applyingVisibleState.current != visible) {
+            theAnimation.current?.stop()
+            theAnimation.current = null
+        }
+        applyingVisibleState.current = visible
+
+        theAnimation.current = Animated.timing(visibilityAnimValue, {
+            toValue: visible ? 1 : 0,
+            duration,
+            easing: Easing.inOut(Easing.linear),
+            useNativeDriver: true
+        })
+
+        if (visible && !visibleState) { setVisibleState(true) }
+        theAnimation.current.start(({ finished }) => {
+            theAnimation.current = null
+            if (finished) {
+                setVisibleState(visible)
+                animationFinished && animationFinished(visible)
+            }
+        })
+    }, [visible])
+
+    var transforms: any[] = []
+
+    if (entranceBearing != Bearing.Center || (applyingVisibleState.current == false && leaveBearing == entranceBearing)) {
+        if (entranceBearing == Bearing.Left || entranceBearing == Bearing.Right)
+            transforms.push({ translateX: visibilityAnimValue.interpolate({ inputRange: [0, 1], outputRange: [entranceBearing == Bearing.Left ? -50 : 50, 0] }) })
+
+        if (entranceBearing == Bearing.Top || entranceBearing == Bearing.Bottom)
+            transforms.push({ translateY: visibilityAnimValue.interpolate({ inputRange: [0, 1], outputRange: [entranceBearing == Bearing.Top ? -50 : 50, 0] }) })
+    } else if (applyingVisibleState.current == false && leaveBearing != Bearing.Center) {
+        if (leaveBearing == Bearing.Left || leaveBearing == Bearing.Right)
+            transforms.push({ translateX: visibilityAnimValue.interpolate({ inputRange: [0, 1], outputRange: [leaveBearing == Bearing.Left ? -50 : 50, 0] }) })
+
+        if (leaveBearing == Bearing.Top || leaveBearing == Bearing.Bottom)
+            transforms.push({ translateY: visibilityAnimValue.interpolate({ inputRange: [0, 1], outputRange: [leaveBearing == Bearing.Top ? -50 : 50, 0] }) })
+    }
+
+    var containerStyle = {
+        opacity: visibilityAnimValue,
+        transform: [
+            ...transforms,
+            {
+                scale: visibilityAnimValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [fadeOutScale, 1],
+                })
+            }
+        ]
+    }
+
+    return (
+        <Animated.View pointerEvents={visibleState ? 'auto' : 'none'} style={[style, containerStyle]} {...rest}>
+            { visibleState && children }
+        </Animated.View>
+    )
+}
+
+export default FadeView
